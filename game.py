1c0145aae16cec316d0177dafde76264d4b207af
Andrey Dubov <andrey123829@yandex.ru>
2025-08-08 09:31:40 +0300

    Do with FieldIndexError

--- game.py
+++ game.py
@@ -8,12 +8,25 @@ from gameparts.exceptions import FieldIndexError
 def main():
     game = Board()
     game.display()
-    row = int(input('Введите номер строки: '))
-    if row < 0 or row >= game.field_size:
-        raise FieldIndexError('Введенное значение за границами игрового поля!')
-    column = int(input('Введите номер столбца: '))
-    if column < 0 or column >= game.field_size:
-        raise FieldIndexError('Введенное значение за границами игрового поля!')
+
+    while True:
+        try:
+            row = int(input('Введите номер строки: '))
+            if row < 0 or row >= game.field_size:
+                raise FieldIndexError
+            column = int(input('Введите номер столбца: '))
+            if column < 0 or column >= game.field_size:
+                raise FieldIndexError
+        except FieldIndexError:
+            print(
+                'Значение должно быть неотрицательный и меньше '
+                f'{game.field_size}.'
+            )
+            print('Пожалуйста, введите значение для строки или столбца заново.')
+            continue
+        else:
+            break
+
     game.make_move(row, column, 'X')
     print('Ход сделан!')
     game.display()
