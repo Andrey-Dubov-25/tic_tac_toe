eb276b35ff9f433eb35cc588282740767899dd2c
Andrey Dubov <andrey123829@yandex.ru>
2025-08-08 09:04:35 +0300

    Add check on out of range

--- game.py
+++ game.py
@@ -1,13 +1,16 @@
 # game.py
-from inspect import ismethod
-
 from gameparts import Board
+
+
+from gameparts.exceptions import FieldIndexError
 
 # Вот новая функция.
 def main():
     game = Board()
     game.display()
     row = int(input('Введите номер строки: '))
+    if row < 0 or row >= game.field_size:
+        raise FieldIndexError
     column = int(input('Введите номер столбца: '))
     game.make_move(row, column, 'X')
     print('Ход сделан!')
